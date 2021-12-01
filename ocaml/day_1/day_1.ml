a250743299741873dd278e336e9de82bded52351
Julien Girard <julien.girard2@cea.fr>
2021-12-01 23:03:04 +0100

    Day one complete in OCaml

--- ocaml/day_1/day_1.ml
+++ ocaml/day_1/day_1.ml
@@ -2,11 +2,24 @@ open Base
 open Stdio
 
 let () =
-let find_incr l = List.fold ~init:(Int.max_value,0) l ~f:(fun acc x ->
-    let p = fst acc and c = snd acc in
-    if x > p then (x, c+1)
-    else (x, c))
+let find_incr l i = List.fold ~init:i l ~f:(fun acc x ->
+      match acc with
+      | [p; o] -> if x > p then [x; o+1] else [x; o]
+      | [last; mid; first; objective; count] ->
+        if count = 3 then(
+          if mid + first + x > mid + first + last then
+            [mid; first; x; objective +1; 3]
+          else
+            [mid; first; x; objective ; 3]
+        )
+        else
+          [mid; first; x; objective; count + 1]
+      | _ -> failwith "should not happen"
+    )
 in
 let lines = List.map ~f:(Int.of_string) @@
-  In_channel.read_lines "input" in 
-printf "Number of increases: %d\n" (snd (find_incr lines))
+  In_channel.read_lines "input" in
+printf "Number of increases: %d\n"
+  (List.nth_exn  (find_incr lines [Int.max_value;0]) 1);
+printf "Number of increases within measuring windows: %d\n"
+  (List.nth_exn  (find_incr lines [0;0;0;0;0]) 3);
