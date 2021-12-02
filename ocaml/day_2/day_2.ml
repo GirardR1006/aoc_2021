ddc659f1c3622796d5b890ca3b137973e24868e8
Julien Girard <julien.girard2@cea.fr>
2021-12-02 16:48:02 +0100

    Less crude second puzzle

--- ocaml/day_2/day_2.ml
+++ ocaml/day_2/day_2.ml
@@ -6,7 +6,11 @@ type horizontal_pos = X of int
 
 type vertical_pos = Y of int
 
-type pos = horizontal_pos * vertical_pos
+type aim = A of int
+
+type pos =
+  | CrudePosition of horizontal_pos * vertical_pos
+  | RefinedPosition of horizontal_pos * vertical_pos * aim
 
 type direction = Forward | Up | Down
 
@@ -17,12 +21,26 @@ let dir_of_string = function
   | _ -> failwith "Should not happen"
 
 let () =
-  let move_submarine l =
-    List.fold ~init:(X 0, Y 0) l ~f:(fun (X x, Y y) (pos, value) ->
-      match pos with
-      | Forward -> (X (x + value), Y y)
-      | Up -> (X x, Y (y + value))
-      | Down -> (X x, Y (y - value)))
+  let move_submarine l i =
+    let res =
+      List.fold ~init:i l ~f:(fun i (pos, value) ->
+        match i with
+        | CrudePosition (X x, Y y) -> (
+          match pos with
+          | Forward -> CrudePosition (X (x + value), Y y)
+          | Up -> CrudePosition (X x, Y (y + value))
+          | Down -> CrudePosition (X x, Y (y - value)))
+        | RefinedPosition (X x, Y y, A a) -> (
+          match pos with
+          | Forward ->
+            if a = 0
+            then RefinedPosition (X (x + value), Y y, A a)
+            else RefinedPosition (X (x + value), Y (y + (value * a)), A a)
+          | Up -> RefinedPosition (X x, Y y, A (a - value))
+          | Down -> RefinedPosition (X x, Y y, A (a + value))))
+    in
+    match res with
+    | CrudePosition (X x, Y y) | RefinedPosition (X x, Y y, _) -> x * y
   in
   let parse str =
     match String.split ~on:' ' str with
@@ -30,5 +48,7 @@ let () =
     | _ -> failwith "Should also not happen"
   in
   let lines = List.map ~f:parse @@ In_channel.read_lines "input" in
-  let X x, Y y = move_submarine lines in
-  printf "Horizontal position multiplied by depth: %d\n" (x * y)
+  printf "Crude horizontal position multiplied by depth: %d\n"
+    (move_submarine lines (CrudePosition (X 0, Y 0)));
+  printf "Horizontal position multiplied by depth after RTFM: %d\n"
+    (move_submarine lines (RefinedPosition (X 0, Y 0, A 0)))
